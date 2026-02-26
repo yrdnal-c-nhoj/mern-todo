5927715b49c8112cb5786fd46fc45746e0d59c88
john-c-landry <johnclandry@yahoo.com>
2026-02-26 16:26:21 -0500

    z

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const cors = require("cors");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Credentials', 'false');
   
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
